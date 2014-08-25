4246bb9ec1a3d6c4ced7aa14fcd9a9ea449b56c7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-08-25 22:13:54 +0900

    m
    Initial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#include <ode/ode.h>
+#include <drawstuff/drawstuff.h>
+#include "texturepath.h"
+
+int main()
+{
+
+    dsFunctions fn;
+
+    cout << "Hello world!" << endl;
+
+
+
+    return 0;
+}
